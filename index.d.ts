50709d80650a152a9e4fcffef7fce08e08b57248
Akira Tetsuka <eharmonicminorperfect5thbelow@gmail.com>
2020-03-15 20:21:00 +0900

    bug fix

--- index.d.ts
+++ index.d.ts
@@ -3,12 +3,12 @@ import { FC, ReactNode } from 'react';
 
 declare module '@matejmazur/react-katex' {
   export interface TeXProps {
-    children: string;
-    math: string;
-    block: boolean;
-    errorColor: string;
-    renderError: (error: ParseError | TypeError) => ReactNode;
-    settings: KatexOptions;
+    children?: string;
+    math?: string;
+    block?: boolean;
+    errorColor?: string;
+    renderError?: (error: ParseError | TypeError) => ReactNode;
+    settings?: KatexOptions;
   }
 
   const TeX: FC<TeXProps>;
